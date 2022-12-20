140a7d6fb8727def76ea9c124de413ddc00627c6
viniciusClunc <vinicius@valorizza.com.br>
2022-12-20 16:42:33 -0300

    First and last commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { Pen, PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1579403124614-197f69d8187b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/vinnic77.png" />
+        <strong>Vinícius Clunc</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
